74ac9928b2f531085f197a29b5ac94dc9b0374dc
Abdulrahman Khaled <AK-Salah@outlook.com>
2024-02-19 07:57:43 -0800

    Added task '2. Export to JSON'
    Extends the python script of task '0. Gather data from an API' to export data in the JSON format.
    
    Requirements:
    - Records all tasks that are owned by this employee
    - Format must be: { 'USER_ID': [{'task': 'TASK_TITLE', 'completed': TASK_COMPLETED_STATUS, 'username': 'USERNAME'}, {'task': 'TASK_TITLE', 'completed': TASK_COMPLETED_STATUS, 'username': 'USERNAME'}, ... ]}
    - File name must be: 'USER_ID.json'
    
    Files:
    - 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Export data from an API to JSON format.
+"""
+from json import dumps
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    # Checks if the argument can be converted to a number
+    try:
+        emp_id = int(argv[1])
+    except ValueError:
+        exit()
+
+    # Main formatted names to API uris and filenames
+    api_url = 'https://jsonplaceholder.typicode.com'
+    user_uri = '{api}/users/{id}'.format(api=api_url, id=emp_id)
+    todo_uri = '{user_uri}/todos'.format(user_uri=user_uri)
+    filename = '{emp_id}.json'.format(emp_id=emp_id)
+
+    # User Response
+    user_res = requests.get(user_uri).json()
+
+    # User TODO Response
+    todo_res = requests.get(todo_uri).json()
+
+    # The initial list & the for loop to fill it
+    user_list = list()
+    for elem in todo_res:
+        data = {
+            "task": elem.get('title'),
+            "completed": elem.get('completed'),
+            "username": user_res.get('username')
+        }
+
+        user_list.append(data)
+
+    # Create the new file for the user to save the information
+    # Filename example: `{user_id}.json`
+    with open(filename, 'w', encoding='utf-8') as jsonfile:
+        jsonfile.write(dumps({emp_id: user_list}))
